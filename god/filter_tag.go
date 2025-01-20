ef5bdab03522c020fbd318629bbb9e706a46805d
Meaww2 <childenchaos@gmail.com>
2025-01-20 08:37:33 +0700

    fix: change logical symbol (v1.1.12)

--- god/filter_tag.go
+++ god/filter_tag.go
@@ -35,19 +35,19 @@ func Filter_tag(html_ch chan HTMLcontent, content_ch chan DBobj) {
 		temp := ""
 		for i < len(body) {
 			// skip comment
-			if len(body) >= 4 || body[0:4] == "<!--" {
+			if len(body) >= 4 && body[0:4] == "<!--" {
 				i = strings.Index(body, "-->")
 				body = body[i+3:]
 				continue
 			}
 			// skip script
-			if len(body) >= 8 || body[0:8] == "<script>" {
+			if len(body) >= 8 && body[0:8] == "<script>" {
 				i = strings.Index(body, "</script>")
 				body = body[i+8:]
 				continue
 			}
 			// skip style
-			if len(body) >= 6 || body[0:6] == "<style" {
+			if len(body) >= 6 && body[0:6] == "<style" {
 				i = strings.Index(body, "</style")
 				body = body[i+6:]
 				i = strings.Index(body, ">")
@@ -55,7 +55,7 @@ func Filter_tag(html_ch chan HTMLcontent, content_ch chan DBobj) {
 				continue
 			}
 			// skip reference ex. [1]
-			if len(body) >= 10 || body[0:10] == "<spanclass" {
+			if len(body) >= 10 && body[0:10] == "<spanclass" {
 				i = strings.Index(body[10:], "<spanclass")
 				body = body[i+20:]
 				i = strings.Index(body, "</span>")
